64c8f141f76a2d617e86df9ced3e7b1f5ae3dacb
Marcel Aguiar <marcelaguiar9@gmail.com>
2020-10-27 13:01:32 -0700

    added some comments

--- program.py
+++ program.py
@@ -6,16 +6,16 @@ import time
 def main():
     base_url = 'https://www.recreation.gov/api/camps/availability/campground/'
     
-    # Specify a browser to the endpoint
+    # Specify a browser to the endpoint just so it agrees
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
 
     target_campgrounds = [
         232470,
+        232472,
         232473,
         272299,
-        232472,
-        10056207,
-        272300
+        272300,
+        10056207
     ]
 
     target_dates = [
@@ -23,9 +23,6 @@ def main():
         datetime.datetime(2020, 11, 14),
         datetime.datetime(2020, 11, 15)
     ]
-
-
-
 
     for campground_id in target_campgrounds:
         target_url = base_url + str(campground_id) + '/month'
@@ -37,6 +34,9 @@ def main():
 
         r = requests.get(target_url, params=payload, headers=headers)
         print(r.text)
+
+        # Parse response and check against target_dates
+        # Use twitter for fastest alerts
 
 
 if __name__ == "__main__":
